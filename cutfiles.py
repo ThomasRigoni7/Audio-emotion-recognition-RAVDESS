749315580f08ee5c94c0ed2e533cd053f3d4b661
Thomas Rigoni <thomas.rigoni7@gmail.com>
2021-02-23 19:08:13 +0100

    added mel-mfcc generator

--- cutfiles.py
+++ cutfiles.py
@@ -1,42 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 16 16:48:01 2020
-
-@author: iv3r0
-"""
-
-
-
 import soundfile as sf
-import numpy
+import numpy as np
 import glob   
 import os 
 import matplotlib.pyplot as plot
 import librosa 
+import argparse
+
+parser = argparse.ArgumentParser(description='Desciption')
+parser.add_argument('-l', '--maxlen', type = int, help = "maximum file length", default=128000)
+
+arg = parser.parse_args()
+max_len = arg.maxlen
 
 f_files=[]
 fcut_files=[]
 
-for i, file in enumerate(glob.glob('RAVDESS_dataset/wav/**/**/*.wav')):
-        
-        print(i,end='\r')
-        f,sr = sf.read(file) 
-        f_files.append(len(f))
-        
-        #plot.plot(f)
-        #plot.show()
-        #print(file,i)
-        dir_name = os.path.dirname(file)
-        dir_name_cut = dir_name.replace('wav','wavcut') 
-        
-        os.makedirs(dir_name_cut, exist_ok=True)
-        filename = os.path.basename(file)
-        
-        fcut,index= librosa.effects.trim(f,frame_length=2098, hop_length=562)  ### DEFAULT  frame_length=2048, hop_length=512
+for i, file in enumerate(glob.glob('RAVDESS_dataset/wav_original/**/**/*.wav')):
+    print(i,end='\r')
+    f, sr = librosa.load(file)
+    f_files.append(len(f))
     
-        fcut_files.append(len(fcut))
-      
-        sf.write(dir_name_cut+'/'+ filename, fcut, sr)                         ##creation of filename in new folder 
+    dir_name = os.path.dirname(file)
+    dir_name_mfcc = dir_name.replace('wav_original','mfcc') 
+    dir_name_mel = dir_name.replace('wav_original','mels') 
+    
+    os.makedirs(dir_name_mfcc, exist_ok=True)
+    os.makedirs(dir_name_mel, exist_ok=True)
+    filename = os.path.basename(file)
+    new_filename = filename.replace('.wav', '')
+    
+    fcut,index= librosa.effects.trim(f,frame_length=2048, hop_length=512)  ### DEFAULT  frame_length=2048, hop_length=512
+    fcut_files.append(len(fcut))
+
+    fcut = fcut[:max_len]
+    if len(fcut) < max_len: 
+        ff = np.pad(fcut, [(0, max_len - fcut.shape[0]),], mode='constant')
+    waveform=ff
+    mfcc = librosa.feature.mfcc(y=waveform, sr=sr, n_mfcc=40)
+    mel_spectogram = librosa.feature.melspectrogram(y=waveform, sr=sr, n_mels=40)
+    scaled_mel = librosa.power_to_db(mel_spectogram)
+    
+    np.save(dir_name_mfcc+'/'+ filename, mfcc)
+    np.save(dir_name_mel+'/'+filename, scaled_mel)
         
 
 ### plotting histogram
