4871462bc81bfccb18ed0d826b8506741a935c99
Thomas Rigoni <thomas.rigoni7@gmail.com>
2021-03-18 18:12:50 +0100

    added suffix manipulation and transformations in data loader + refactor

--- dataset_ravdess.py
+++ dataset_ravdess.py
@@ -8,29 +8,66 @@ import random
 from pathlib import Path
 
 
+
+
 class RAVDESS_DATA(data.Dataset):
-    def __init__(self, csv_path, device, data_dir="./RAVDESS_dataset/mels/", chunk_len=153, random_load=True):
-        super(RAVDESS_DATA, self).__init__()
-        self.chunk_len = chunk_len
-        self.random_load = random_load
+    def _load_csv(self, csv_path):
         try:
             data = pd.read_csv(csv_path)
         except OSError as oserr:
                 raise RuntimeError(
                     "csv file not found in '{}', did you download the dataset and generate the files with genfiles.py?".format(csv_path)) from oserr
-        filenames = data.values.tolist()
-        self.files = []
+        return data.values.tolist()
+    
+    def _load_files(self, filenames):
+        print("Loading dataset...")
+        files = []
         for file, label in filenames:
             filepath = Path(file)
-            if data_dir is not None:
-                filepath = Path(data_dir) / filepath
+            if self.data_dir is not None:
+                filepath = (Path(self.data_dir) / filepath).with_suffix(self.in_suffix)
             try:
                 f = open(filepath, 'rb')
             except OSError as oserr:
                 raise RuntimeError(
                     "Data file not found in '{}', did you download the dataset and generate the files with genfiles.py?".format(filepath)) from oserr
             with f:
-                self.files.append((torch.load(f), int(label) - 1))
+                if self.in_suffix == ".pt":
+                    x = torch.load(f)
+                elif self.in_suffix == ".wav":
+                    x, sr = librosa.load(f)
+                else:
+                    raise RuntimeError(
+                    "The suffix '{}' is not valid.".format(self.in_suffix)) from oserr
+                if self.transformations == "none":
+                    pass
+                elif self.transformations == "mel":
+                    x = librosa.feature.melspectrogram(
+                        y=waveform, sr=sr, n_mels=40)
+                    x = librosa.power_to_db(mel_spectogram)
+                else:
+                    raise RuntimeError(
+                    "The transformations '{}' are not valid.".format(self.transformations)) from oserr
+                if self.in_suffix == ".wav":
+                    x = torch.from_numpy(x)
+                if self.device is not None:
+                    x.to(self.device)
+                files.append((x, int(label) - 1))
+        print("DONE")
+        return files
+    
+    def __init__(self, csv_path, device=None, data_dir="./RAVDESS_dataset/mels/", chunk_len=153, random_load=True, in_suffix=".pt", transformations="none"):
+        super(RAVDESS_DATA, self).__init__()
+        self.chunk_len = chunk_len
+        self.random_load = random_load
+        self.data_dir = data_dir
+        self.csv_path = csv_path
+        self.device = device
+        self.in_suffix = in_suffix
+        self.transformations = transformations
+        filenames = self._load_csv(csv_path)
+        self.files = self._load_files(filenames)
+        
 
     def __len__(self):
         return len(self.files)
